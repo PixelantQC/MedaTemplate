20227fc23b505123001269a87e0a9537c43bda18
codterpin <anton.perebyinis@gmail.com>
2015-12-15 19:19:57 +0200

    Switching to saucelabs automation testing

--- Nightwatch/scripts/globals.js
+++ Nightwatch/scripts/globals.js
@@ -17,20 +17,20 @@ module.exports = {
       .waitForElementVisible(modal, 3000, 'HCP modal show up?')
       .click(confirm)
       .pause(2000)
-      .waitForElementVisible(productView, 1000, 'Product view show up?')
+      .waitForElementVisible(productView, 2000, 'Product view show up?')
   },
 
   goToProductInfo: function (browser, productNumber, productUrl) {
     browser
       .click('.products-list-view-wrapper .item:nth-child(' + productNumber + ')')
       .pause(2000)
-      .waitForElementVisible(productContent, 1000, 'Product content show up?')
+      .waitForElementVisible(productContent, 2000, 'Product content show up?')
       .assert.cssClassPresent('.products-list-view-wrapper .item:nth-child(' + productNumber + ')', 'active', 'First item is active now?')
       .click(productLink)
       .pause(2000)
-      .waitForElementVisible('body', 1000, 'Product info page is loaded?')
+      .waitForElementVisible('body', 2000, 'Product info page is loaded?')
       .assert.urlContains(productUrl, 'Are we on product info page?')
       .execute(function () { window.history.back() })
-      .waitForElementVisible('body', 1000, 'Got back to all products page?')
+      .waitForElementVisible('body', 2000, 'Got back to all products page?')
   }
 }
